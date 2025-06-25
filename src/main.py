20dbd0b2d875419a62a6cdf6a6695d82f3693a0a
Gleb <fedorov22134@gmail.com>
2025-06-26 01:50:42 +0300

    add method for test models

--- src/main.py
+++ src/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI, Depends
 
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from src.config.core.database_helper import database_helper
+from src.config.models.restaurant import Restaurant
+
 
 app = FastAPI()
 
 @app.get("/")
-async def root():
+async def root(session: AsyncSession = Depends(database_helper.session_depends)):
+    stmt = select(Restaurant)
+    result = await session.execute(stmt)
+    restaurants = result.scalars().all()
+    for restaurant in restaurants:
+        print(restaurant.name)
     return {"message": "Hello world"}
