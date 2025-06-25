d4b72e50f767ef14df9db518a7c67b2133ce0ba9
Gleb <fedorov22134@gmail.com>
2025-06-26 02:08:39 +0300

    fix bug with model dish

--- src/config/models/dish.py
+++ src/config/models/dish.py
@@ -1,21 +1,23 @@
 import uuid
-from sqlalchemy import String, Float, Boolean, ForeignKey, Integer, Table
+
+from sqlalchemy import String, Float, Boolean, ForeignKey, Table, Column
 from sqlalchemy.dialects.postgresql import ARRAY
 from sqlalchemy.orm import Mapped, mapped_column, relationship
+
 from src.config.core.base import Base
 
 dish_ingredient_association = Table(
     "restoran_dish_ingredients",
     Base.metadata,
-    mapped_column("dish_id", ForeignKey("restoran_dish.id"), primary_key=True),
-    mapped_column("ingredient_id", ForeignKey("restoran_ingredients.id"), primary_key=True)
+    Column("dish_id", ForeignKey("restoran_dish.id"), primary_key=True),
+    Column("ingredient_id", ForeignKey("restoran_ingredients.id"), primary_key=True)
 )
 
 dish_label_association = Table(
     "restoran_dish_label",
     Base.metadata,
-    mapped_column("dish_id", ForeignKey("restoran_dish.id"), primary_key=True),
-    mapped_column("label_id", ForeignKey("restoran_label.id"), primary_key=True)
+    Column("dish_id", ForeignKey("restoran_dish.id"), primary_key=True),
+    Column("label_id", ForeignKey("restoran_label.id"), primary_key=True)
 )
 
 
@@ -27,15 +29,15 @@ class Dish(Base):
     rkeeper_code_product: Mapped[int | None] = mapped_column(nullable=True)
     rkeeper_GUIDString: Mapped[str | None] = mapped_column(String(255), nullable=True)
 
-    id_rest: Mapped[uuid.UUID] = mapped_column(ForeignKey("restoran_restaurant.id"))
+    id_rest_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("restoran_restaurant.id"))
 
-    cat_id: Mapped[uuid.UUID | None] = mapped_column(ForeignKey("restoran_category.id"), nullable=True)
+    cat_id_id: Mapped[uuid.UUID | None] = mapped_column(ForeignKey("restoran_category.id"), nullable=True)
 
     name: Mapped[str] = mapped_column(String(500), nullable=False)
     description: Mapped[str] = mapped_column(String(500), nullable=False)
 
-    ingredients = relationship("restoran_ingredients", secondary=dish_ingredient_association, back_populates="dishes")
-    labels = relationship("restoran_label", secondary=dish_label_association, back_populates="dishes")
+    ingredients = relationship("Ingredient", secondary=dish_ingredient_association, back_populates="dishes")
+    labels = relationship("Label", secondary=dish_label_association, back_populates="dishes")
 
     weight: Mapped[float | None] = mapped_column(nullable=True)
     unit: Mapped[str | None] = mapped_column(String(255), nullable=True)
@@ -56,4 +58,8 @@ class Dish(Base):
     show: Mapped[bool] = mapped_column(default=True)
 
     def __repr__(self):
-        return f"<Dish(name='{self.name}', restaurant='{self.id_rest}')>"
+        return f"<Dish(name='{self.name}', restaurant='{self.id_rest_id}')>"
+
+
+from src.config.models.ingredient import Ingredient
+from src.config.models.label import Label
