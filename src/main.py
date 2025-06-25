12330bb15e6f5f51568f2e423e193080fd050ad3
Gleb <fedorov22134@gmail.com>
2025-06-26 02:09:11 +0300

    show dish instead of restaurant

--- src/main.py
+++ src/main.py
@@ -4,15 +4,16 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
 from src.config.core.database_helper import database_helper
 from src.config.models.restaurant import Restaurant
+from src.config.models.dish import Dish
 
 
 app = FastAPI()
 
 @app.get("/")
 async def root(session: AsyncSession = Depends(database_helper.session_depends)):
-    stmt = select(Restaurant)
+    stmt = select(Dish)
     result = await session.execute(stmt)
-    restaurants = result.scalars().all()
-    for restaurant in restaurants:
-        print(restaurant.name)
+    dishes = result.scalars().all()
+    for dish in dishes:
+        print(dish.name)
     return {"message": "Hello world"}
