a315038cb4e42147fc46e5bc55bc2a4753797f33
Gleb <fedorov22134@gmail.com>
2025-06-26 01:20:05 +0300

    add structure for paths with images

--- src/config/core/settings.py
+++ src/config/core/settings.py
@@ -8,7 +8,20 @@ class DataBaseSettings(BaseModel):
     url: str = os.getenv("DATABASE_URL")
     echo: bool = os.getenv("DATABASE_ECHO") == "True"
 
+class FileSettings(BaseSettings):
+    path_create_upload_fir: str = "src/statis/uploads/"
+    path_logo: str = path_create_upload_fir + "logo/"
+    path_avatar: str = path_create_upload_fir + "avatar/"
+    path_img: str = path_create_upload_fir + "img/"
+
 class Settings(BaseSettings):
     database_settings: DataBaseSettings = DataBaseSettings()
+    file_settings: FileSettings = FileSettings()
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for dirpath in self.file_settings.model_dump():
+            os.makedirs(dirpath, exist_ok=True)
 
 settings = Settings()
