68914135731df3c819b6e70996ebb6fc622c493b
Gleb <fedorov22134@gmail.com>
2025-06-25 20:24:13 +0300

    add database helper for session and work with db

---
+++ src/config/core/database_helper.py
@@ -0,0 +1,25 @@
+from sqlalchemy.ext.asyncio import (
+    create_async_engine, async_sessionmaker, AsyncSession
+)
+
+from typing import AsyncGenerator
+
+
+class DataBaseHelper:
+    AUTOFLUSH = False
+    AUTOCOMMIT = False
+    EXPIRE_ON_COMMIT = False
+
+    def __init__(self, url: str, echo: bool):
+        self.engine = create_async_engine(url, echo=echo)
+
+        self.session_maker = async_sessionmaker(
+            bind=self.engine,
+            autocommit=self.AUTOCOMMIT,
+            expire_on_commit=self.EXPIRE_ON_COMMIT,
+            autoflush=self.AUTOFLUSH,
+        )
+
+    async def session_depends(self) -> AsyncGenerator[AsyncSession, None]:
+        async with self.session_maker() as session:
+            yield session
