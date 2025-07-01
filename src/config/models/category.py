c41b7a4e3e5c86a66dbe76c7525db163c40da5d6
Gleb <fedorov22134@gmail.com>
2025-07-01 23:24:13 +0300

    fix bug in categories model

--- src/config/models/category.py
+++ src/config/models/category.py
@@ -7,19 +7,19 @@ from src.config.core.base import Base
 class Category(Base):
     __tablename__ = "restoran_category"
 
-    id_category_iiko: Mapped[Optional[str]] = mapped_column(String(255), nullable=True)
+    id_category_iiko: Mapped[Optional[str]] = mapped_column(String(255), default="", nullable=True)
 
     name: Mapped[str] = mapped_column(String(500), nullable=False)
     parrent_id: Mapped[Optional[uuid.UUID]] = mapped_column(ForeignKey("restoran_category.id"), nullable=True)
 
     parrent: Mapped[Optional["Category"]] = relationship("Category", remote_side="[Category.id]", backref="children")
 
-    des: Mapped[Optional[str]] = mapped_column(String(500), nullable=True)
+    des: Mapped[Optional[str]] = mapped_column(String(500), default="", nullable=True)
     show: Mapped[bool] = mapped_column(Boolean, default=True)
 
-    photo: Mapped[Optional[str]] = mapped_column(String(255), nullable=True)
+    photo: Mapped[Optional[str]] = mapped_column(String(255), default="", nullable=True)
 
-    id_rest: Mapped[uuid.UUID] = mapped_column(ForeignKey("restoran_restaurant.id"), nullable=False)
+    id_rest_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("restoran_restaurant.id"), nullable=False)
 
     def __repr__(self):
         return f"<Category(name='{self.name}', id='{self.id}')>"
