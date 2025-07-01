2b5b6babb3a7784109d0f1a623e8f5a3414a89ce
Gleb <fedorov22134@gmail.com>
2025-07-01 16:23:41 +0300

    add functionality for repository

--- src/repository/repository.py
+++ src/repository/repository.py
@@ -13,13 +13,13 @@ class BaseRepository(Generic[ModelType, SessionType], ABC):
     def __init__(self, model: Type[ModelType]):
         self.model = model
 
-    async def get_by_id(self, session: Type[SessionType], id_: str) -> ModelType:
+    async def get_by_id(self, session: SessionType, id_: str) -> ModelType:
         stmt = select(self.model).where(self.model.id==id_)
         records = await self._get_result(session, stmt)
         return self._get_first_record(records)
 
     async def get_all(
-            self, session: Type[SessionType], offset: int = 0, limit: int = 10
+            self, session: SessionType, offset: int = 0, limit: int = 10
     ) -> Tuple[int, Sequence[ModelType]]:
         stmt = select(self.model).offset(offset).limit(limit)
         stmt_count = select(func.count()).select_from(self.model)
@@ -27,25 +27,29 @@ class BaseRepository(Generic[ModelType, SessionType], ABC):
         count_records: Result = await session.execute(stmt_count)
         return count_records.scalar_one(), records
 
-    async def add(self, session: Type[SessionType], data_add: Dict[str, any]) -> ModelType:
+    async def add(self, session: SessionType, data_add: Dict[str, any]) -> ModelType:
+        found, instance = self._get_if_already_exists(session, data_add)
+        if found:
+            return instance
+
         instance = self.model(**data_add)
         session.add(instance)
         await self._commit_and_refresh_session(session, instance)
         return instance
 
-    async def update(self, session: Type[SessionType], id_: str, data_update: Dict[str, any]) -> ModelType:
+    async def update(self, session: SessionType, id_: str, data_update: Dict[str, any]) -> ModelType:
         instance = self.get_by_id(session, id_)
         updated_instance = self._update_data_in_instance(instance, data_update)
         await self._commit_and_refresh_session(session, instance)
         return updated_instance
 
-    async def delete(self, session: Type[SessionType], id_: str) -> None:
+    async def delete(self, session: SessionType, id_: str) -> None:
         instance = self.get_by_id(session, id_)
         await session.delete(instance)
         await self._commit_and_refresh_session(session)
 
     @staticmethod
-    async def _get_result(session: Type[SessionType], stmt: Select) -> Sequence[ModelType]:
+    async def _get_result(session: SessionType, stmt: Select) -> Sequence[ModelType]:
         result: Result = await session.execute(stmt)
         return result.scalars().all()
 
@@ -56,7 +60,7 @@ class BaseRepository(Generic[ModelType, SessionType], ABC):
         raise ValueError("Not found records")
 
     @staticmethod
-    async def _commit_and_refresh_session(session: Type[SessionType], instance: ModelType | None = None) -> None:
+    async def _commit_and_refresh_session(session: SessionType, instance: ModelType | None = None) -> None:
         await session.commit()
         if instance is not None:
             await session.refresh(instance)
@@ -67,3 +71,12 @@ class BaseRepository(Generic[ModelType, SessionType], ABC):
             if value is not None:
                 setattr(instance, key, value)
         return instance
+
+    async def _get_if_already_exists(
+            self, session: SessionType, data_add: Dict[str, any]
+    ) -> Tuple[bool, ModelType | None]:
+        stmt = select(self.model).where(**data_add)
+        result = await session.execute(stmt)
+        if instance := result.scalars().first():
+            return True, instance
+        return False, None
